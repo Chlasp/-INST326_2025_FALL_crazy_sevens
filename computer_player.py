24333e3896d2a6b586f018696f4c90ec719a1145
Your Name <brian.gardnerbusiness45@gmail.com>
2025-11-14 20:08:18 -0500

    Completed Computer Turn Class

--- computer_player.py
+++ computer_player.py
@@ -1,23 +1,43 @@
 #Computer Player will be an object with a current deck of cards
 import random
-def computer_turn(computer_player, last_value):
-    # Dummy Values
-    computer_player.hand =({"Ace of Spades": random.choice(1 or 11),
-                            "Seven of Hearts": 7, "Seven of Diamonds": 7,
-                           "Six of Spades": 6, "King of Diamonds": 10})
-    # TODO Sort Values only if they are above the card Value using List Comprehension. 
-    playable_hand = [card for card in computer_player.hand
-                            if card.value > last_value]
-    if not playable_hand:
-        return None
-    # TODO check to see if any cards have duplicates
-    
-    # TODO Check to see if a card has three or more cards above card number + 3
-    
-    
-     
+import math
+def computer_turn(computer_player, last_card, round):
+    """Runs a computer player's turn 
 
-# The cards are currently played in an ongoing round.
-# The value of the more recently played card.
-# The card with the highest value currently.
-# Computer turn and hand content
+    Args:
+        computer_player (Computer): Computer player object
+        last_card (int): value of the last card played in the game
+        round (int): current round in the game
+    Side Effects:
+        Updates computer_player.hand removing cards played this turn
+    Raises:
+        None
+    Returns:
+        list: Returns a list of cards the player has run
+    """
+    # Dummy Values
+    computer_player.hand = [7,8,8,8,9,10]
+    playable_hand = [card for card in sorted(computer_player.hand) \
+        if card > last_card]
+    count = {}
+    for card in playable_hand:
+        count[card] = count.get(card, 0) + 1
+    double_hand = [card for card in playable_hand if count[card] > 1]
+    if round == 3:
+        if double_hand:
+            chosen = double_hand[0]
+            computer_player.hand = [x for x in playable_hand if x != chosen]
+            return [x for x in playable_hand if x == chosen]
+        chosen = playable_hand[0]
+        computer_player.hand = [x for x in playable_hand if x != chosen]
+        return [x for x in playable_hand if x == chosen]
+    else:
+        if (playable_hand[0] - last_card) < 3:
+            chosen = playable_hand[0]
+            computer_player.hand = [x for x in playable_hand if x != chosen]
+            return [x for x in playable_hand if x == chosen]
+        else:
+            chosen = double_hand[0]
+            computer_player.hand = [x for x in playable_hand if x != chosen]
+            return [x for x in playable_hand if x == chosen]
+    

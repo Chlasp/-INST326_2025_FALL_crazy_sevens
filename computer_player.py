ef1c7e4695e2bde4d3da9b534df7b6ac63591a5b
Your Name <brian.gardnerbusiness45@gmail.com>
2025-11-14 20:36:04 -0500

    Simplified code and added comments for readability

--- computer_player.py
+++ computer_player.py
@@ -13,31 +13,42 @@ def computer_turn(computer_player, last_card, round):
     Raises:
         None
     Returns:
-        list: Returns a list of cards the player has run
+        None
     """
-    # Dummy Values
-    computer_player.hand = [7,8,8,8,9,10]
-    playable_hand = [card for card in sorted(computer_player.hand) \
+    # Initalizes computer player hand
+    hand = computer_player.hand
+    
+    # Creates playable card options
+    playable_hand = [card for card in sorted(hand) \
         if card > last_card]
+    if not playable_hand:
+        return None
+    # Count occurences of each card and check and make a list for doubles
+    # Doubles are card values that occur more than once
     count = {}
     for card in playable_hand:
         count[card] = count.get(card, 0) + 1
     double_hand = [card for card in playable_hand if count[card] > 1]
+    """
+    Logic:
+    If it is currently round three, then immediatly check for doubles.
+    If there are no doubles, than look for the lowest value in
+    the playable hand. 
+    If it is round 2 or less, look first to see if the closest card is within 
+    three values of the target. If it is not then look for double cards first.
+    
+    """
     if round == 3:
         if double_hand:
             chosen = double_hand[0]
             computer_player.hand = [x for x in playable_hand if x != chosen]
-            return [x for x in playable_hand if x == chosen]
         chosen = playable_hand[0]
         computer_player.hand = [x for x in playable_hand if x != chosen]
-        return [x for x in playable_hand if x == chosen]
     else:
         if (playable_hand[0] - last_card) < 3:
             chosen = playable_hand[0]
             computer_player.hand = [x for x in playable_hand if x != chosen]
-            return [x for x in playable_hand if x == chosen]
         else:
             chosen = double_hand[0]
             computer_player.hand = [x for x in playable_hand if x != chosen]
-            return [x for x in playable_hand if x == chosen]
     

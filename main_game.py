2d8288f716ba81da006061f4751b7467fc17a679
Your Name <brian.gardnerbusiness45@gmail.com>
2025-11-21 10:53:22 -0500

    Created card deck and main game file

---
+++ main_game.py
@@ -0,0 +1,12 @@
+class Main():
+    """
+    Card deck 
+    Use Format Card Name : Value dictionary for Kings(K), Aces(A), Jack(J), Q(Queen)
+    """
+    pass
+deck = {"Ace": 1, "2":2, "3":3, "4": 4, "5":5, "6":6, "7":7, "8":8, "9":9, "King":10, "Jack": 11, "Queen": 12}
+deck = []
+card = {"A": 1}
+card = 2
+card = "A"
+

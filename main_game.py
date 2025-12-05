2bd0c7eca42a1ce7964996679e0964311ff4c2e6
Your Name <brian.gardnerbusiness45@gmail.com>
2025-12-05 15:40:33 -0500

    Updated Deck Concept and created Player and ComputerPlayerClass

--- main_game.py
+++ main_game.py
@@ -1,12 +1,28 @@
+import random
+class Player():
+    pass
+class ComputerPlayer():
+    # Inherits from Player Class
+    pass
 class Main():
     """
     Card deck 
-    Use Format Card Name : Value dictionary for Kings(K), Aces(A), Jack(J), Q(Queen)
+    Use Format Card Name : Value (Ex: "(A): 1)
+    dictionary for Kings(K), Aces(A), Jack(J), Q(Queen)
     """
     pass
-deck = {"Ace": 1, "2":2, "3":3, "4": 4, "5":5, "6":6, "7":7, "8":8, "9":9, "King":10, "Jack": 11, "Queen": 12}
+# Create Deck
+card_values = {"A": 1, "2":2, "3":3, "4": 4, "5":5, "6":6, "7":7, "8":8, "9":9,\
+    "K":10, "J": 11, "Q": 12}
 deck = []
-card = {"A": 1}
-card = 2
-card = "A"
+for card in card_values:
+    for __ in range(4): 
+        deck.append(card)
+deck.random.shuffle()
+# Main Game Loop
+
+
+
+
+#
 
